fa1b5eba30a386165b2673a478fb972ba3cc30f0
Shanuka <shanukathavanesan@gmail.com>
2023-12-08 05:28:21 +0530

    added

--- index.js
+++ index.js
@@ -8,9 +8,17 @@ const express = require('express');
 // Mongoose is a MongoDB ODM i.e (Object database Modelling) that used to translate the code and its representation from MongoDB to the Node. js server.
 // Object data model (ODM) is a term used to describe the process of storing information about an object.  
 const mongoose = require('mongoose');
+// process.env is an object that holds the values of environment variables.
+//DATABASE_URL= It is commonly used to store the connection string for a database, such as MongoDB.
+// const mongoString=declares a constant variable named mongoString and assigns the value of the DATABASE_URL environment variable to it.
+// mongoString=connection string that contains information about how to connect to a database.
 const mongoString = process.env.DATABASE_URL;
 
+// mongoose.connect= method provide by Monggode library for connect to MongoDB.
+// mongoString is assumed to be a variable holding the connection string.
 mongoose.connect(mongoString);
+//mongoose.connection= active connection to the MongoDB database
+//const database=declares a constant variable named database and assigns the Mongoose connection object to it. 
 const database = mongoose.connection;
 
 database.on('error', (error) => {
